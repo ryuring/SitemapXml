5e2e3e57cd86f1eaf3c8da1677e5357b3e4cd91b
ryuring <ryuring55@gmail.com>
2012-07-18 23:10:51 +0900

    出力される sitemap.xml の先頭に空白行が入っていたので修正した

--- views/sitemapxml/admin/sitemap.php
+++ views/sitemapxml/admin/sitemap.php
@@ -18,8 +18,6 @@
  * @license			MIT lincense
  */
 ?>
-
-
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php foreach($datas as $data): ?>
